9ff96cbce61fd40f2f646d6ad1d9607c8a82b122
Tal Uliel <talu@moveo.co.il>
2022-03-10 16:48:23 +0200

    version 1

--- src/components/manager.jsx
+++ src/components/manager.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import "antd/dist/antd.css";
 import mondaySdk from "monday-sdk-js";
+import './manager.css'
 const axios = require("axios");
 
 const ACCESS_TOKEN =
@@ -48,7 +49,6 @@ function ManagerForm() {
   };
 
   const fetchAvailBoard = async (board_id) => {
-
     const graphqlQuery = {
       operationName: "fetchAuthor",
       query: `{boards(ids: ${board_id}) {groups {id title items (limit: 15) {id name column_values {id title text value}}}}}`,
@@ -67,9 +67,15 @@ function ManagerForm() {
         employeesAvail.push({
           personId: item.column_values[0].value,
           name: item.column_values[0].text,
-          date: item.column_values[1].text,
-          shift: item.column_values[2].text ===null? [] : item.column_values[2].text.replace(/\s/g,'').split(","),
-
+          date: item.column_values[2].text,
+          role:
+            item.column_values[1].text === null
+              ? []
+              : item.column_values[1].text.split(","),
+          shift:
+            item.column_values[3].text === null
+              ? []
+              : item.column_values[3].text.replace(/\s/g, "").split(","),
         });
       });
     });
@@ -77,7 +83,7 @@ function ManagerForm() {
 
   const mondayContext = () => {
     const monday = mondaySdk();
-    
+
     monday.listen("context", async (res) => {
       globalItemId = res.data.itemId;
       globalBoardId = res.data.boardId;
@@ -85,7 +91,6 @@ function ManagerForm() {
   };
 
   const fetchShifts = async () => {
-
     const graphqlQuery = {
       operationName: "fetchAuthor",
       query: `query{ boards(ids:${globalBoardId}){items (ids: ${globalItemId}) {id name column_values {id title text}}} }`,
@@ -101,46 +106,71 @@ function ManagerForm() {
 
     shiftItemData = response.data.data;
     response.data.data.boards[0].items.forEach((item) => {
+      console.log(item);
       shifts = {
-        date: item.column_values[2].text,
-        shift: item.column_values[3].text.split(","),
+        date: item.column_values[1].text,
+        shift: item.column_values[2].text.split(","),
+        role:
+          item.column_values[0].text === null
+            ? []
+            : item.column_values[0].text.split(",").map(x=> x.trim()),
       };
     });
-
+ 
   };
 
   const generateEmployees = async () => {
-
     let date = shifts.date;
     let shift = shifts.shift;
+    let role = shifts.role
 
-    let person = employeesAvail.filter( (employee) => employee.date === date && employee.shift.some(r=> shift.indexOf(r) >= 0) );
-    console.log(person)
+    let person = employeesAvail.filter(
+      (employee) =>
+        employee.date === date &&
+        employee.shift.some((r) => shift.indexOf(r) >= 0) &&
+        employee.role.some((r) => role.includes(r))
+    );
 
-    let employees=person.map(x=>{
+    let employees = person.map((x) => {
       let id = x.personId.substring(x.personId.indexOf("id") + 4);
-      const myArray = id.slice( 0, 8);
-      let obj={
-        id:myArray,
-      }
-      return obj
-    })
-    console.log(employees);
+      const myArray = id.slice(0, 8);
+      let obj = {
+        id: myArray,
+      };
+      return obj;
+    });
 
+    let employeesArr = [];
 
-let employeesArr=[]
+    employees.forEach((employee, i) => {
+      employeesArr.push(
+        `{\\\"id\\\" :${employee.id},\\\"kind\\\":\\\"person\\\"}`
+      );
+    });
+let status="";
+if(role){
+  if(role.length ===person.length){
+    console.log("complete")
+    status="Complete"
+  }
+  if(role.length >person.length){
+    console.log("partial")
+    status="Partial"
+  }
 
- employees.forEach((employee, i)=>{
-  employeesArr.push(`{\\\"id\\\" :${employee.id},\\\"kind\\\":\\\"person\\\"}`)
-  
-})
-console.log(employeesArr.toString())
+  if(person.length===0){
+    console.log("none")
+    status="None"
+  }
+}
+    
+
+    console.log(status)
 
     const graphqlQuery = {
       operationName: "fetchAuthor",
-      query:
-     ` mutation {
-        change_multiple_column_values(item_id:${globalItemId}, board_id:${globalBoardId}, column_values: "{\\\"person\\\" : {\\\"personsAndTeams\\\":[${employeesArr.toString()}]}}") {
+      query: ` mutation {
+        change_multiple_column_values(item_id:${globalItemId}, board_id:${globalBoardId}, column_values: "{\\\"person\\\" : {\\\"personsAndTeams\\\":[${employeesArr.toString()}]}, \\\"status6\\\": {\\\"label\\\": \\\"${status}\\\"}}") {
           id
         }
       }`,
@@ -153,104 +183,15 @@ console.log(employeesArr.toString())
       data: graphqlQuery,
     });
 
-
-    status();
+    console.log(response)
   };
-  
 
 
-  const status = async () => {
-console.log(shiftItemData)
-    let availEmployees = [];
-    shiftItemData.boards[0].items.map((item) => {
-      availEmployees.push({
-        roles:
-          item.column_values[1].text !== null
-            ? item.column_values[1].text.split(",")
-            : [],
-        employees:
-          item.column_values[4].text !== ""
-            ? item.column_values[4].text.split(",")
-            : [],
-      });
-    });
-    console.log(availEmployees);
-
-    
-    if (availEmployees[0].employees.length === 0) {
-      console.log("none")
-      const graphqlQuery = {
-        operationName: "fetchAuthor",
-        query:
-          "mutation ($myBoardId:Int!, $myItemId:Int!, $myColumnValues:JSON!) { change_multiple_column_values(item_id:$myItemId, board_id:$myBoardId, column_values: $myColumnValues) { id }}",
-        variables: JSON.stringify({
-          myBoardId: globalBoardId,
-          myItemId: globalItemId,
-          myColumnValues: '{"status1" : {"label" : "None"}}',
-        }),
-      };
-
-      const response = await axios({
-        url: endpoint,
-        method: "post",
-        headers: headers,
-        data: graphqlQuery,
-      });
-
-      console.log(response);
-    }
-
-    else if (availEmployees[0].employees.length !==0 && availEmployees[0].roles.length > availEmployees[0].employees.length) {
-      console.log("partial")
-      const graphqlQuery = {
-        operationName: "fetchAuthor",
-        query:
-          "mutation ($myBoardId:Int!, $myItemId:Int!, $myColumnValues:JSON!) { change_multiple_column_values(item_id:$myItemId, board_id:$myBoardId, column_values: $myColumnValues) { id }}",
-        variables: JSON.stringify({
-          myBoardId: globalBoardId,
-          myItemId: globalItemId,
-          myColumnValues: '{"status1" : {"label" : "Partial"}}',
-        }),
-      };
-
-      const response = await axios({
-        url: endpoint,
-        method: "post",
-        headers: headers,
-        data: graphqlQuery,
-      });
-
-      console.log(response);
-    }
-
-   else if (availEmployees[0].roles.length === availEmployees[0].employees.length) {
-    console.log("complete")
-      const graphqlQuery = {
-        operationName: "fetchAuthor",
-        query:
-          "mutation ($myBoardId:Int!, $myItemId:Int!, $myColumnValues:JSON!) { change_multiple_column_values(item_id:$myItemId, board_id:$myBoardId, column_values: $myColumnValues) { id }}",
-        variables: JSON.stringify({
-          myBoardId: globalBoardId,
-          myItemId: globalItemId,
-          myColumnValues: '{"status1" : {"label" : "Complete"}}',
-        }),
-      };
-
-      const response = await axios({
-        url: endpoint,
-        method: "post",
-        headers: headers,
-        data: graphqlQuery,
-      });
-
-      console.log(response);
-    }
-  };
 
   init();
   return (
-    <div className="container">
-      <Button onClick={generateEmployees}>Generate Employees</Button>
+    <div className="container" id="btnCon">
+      <Button id="generateBtn" onClick={generateEmployees}>Generate Employees</Button>
     </div>
   );
 }
